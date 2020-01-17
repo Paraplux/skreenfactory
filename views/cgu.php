4c673252f965093065f96d49d856b9a4ca5a9bb2
Marc Bouchez <theparaplux@gmail.com>
2020-01-17 20:44:30 +0100

    [Dev Principal] Ajout de la page CGU

--- views/cgu.php
+++ views/cgu.php
@@ -1,6 +1,35 @@
 <?php require_once '../components/header.php'; ?>
         
-        
+<div class="cgu-page">
+    <div class="contact-header">
+        <h1>Mentions legales</h1>
+    </div>
+    
+    <div class="mentions">
+    <h2>Hébergement</h2>
+    <p>Le site internet www.skreenfactory.fr est hébergé par OVH ADRESSE OVH. Pour contacter cet hébégeur, rendez vous à l'adresse : SITE OVH</p>
+
+    <h2>Edition et Création</h2>
+    <p>Marc Bouchez</p>
+    <p>TVA non applicable, article 293B du CGI</p>
+    <p>SIRET : </p>
+    <p>Code APE: 6201Z - Programmation Informatique</p>
+
+    <h2>Droit d'auteur</h2>
+    <p>L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation, copie, diffusion, intégrale ou partielle, du contenu du site SkreenFactory, par quelque procédé que ce soit, est illicite sauf autorisation écrite préalable.</p>
+
+    <h2>Protection des données</h2>
+    <p>Conformément au nouveau réglement général sur la protection des données, vous disposez d'un droit d'accès, de modification, de rectification et de suppresion des données nécessaires au traitement de vos demandes de renseignement ou de devis.</p>
+    <p>Il vous suffit de contactez le gérant du site via le formulaire</p>
+    <p>Les données récoltées sont celles du devis, de la demande d'information ou de la commande, elles seront gardés 1 an et ne seront jamais transférées.</p>
+
+    <h2>Cookies</h2>
+    <p>Le site Skreen Factory utilise divers solutions d'analyse de traffic. Seuls les cookies liés à cet activités sont utilisés.</p>
+
+    <h2>Divers</h2>
+    <p>Skreen Factory n'est pas responsable du contenu des liens externes.</p>
+    </div>
+</div>       
 
 
 <?php require_once '../components/footer.php'; ?>
