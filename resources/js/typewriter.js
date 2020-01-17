fb99276783699abdce7497d47a770df82fce564f
Marc Bouchez <theparaplux@gmail.com>
2020-01-17 11:56:40 +0100

    [Fix] Remplacement des simple quote par des double quote

--- resources/js/typewriter.js
+++ resources/js/typewriter.js
@@ -2,8 +2,8 @@
 
 /*Create a div with a specific class*/
 function createDivWithClass(className) {
-    const div = document.createElement('div')
-    div.setAttribute('class', className)
+    const div = document.createElement("div")
+    div.setAttribute("class", className)
     return div
 }
 
@@ -18,9 +18,9 @@ function typewritter (speed) {
 
 
     //HTML Elements
-    const typewriterElem = document.querySelector('.typewriter')
+    const typewriterElem = document.querySelector(".typewriter")
     const typewriterParent = typewriterElem.parentElement
-    const typewriterContainer = createDivWithClass('typewriter-container')
+    const typewriterContainer = createDivWithClass("typewriter-container")
 
     //Variables
     const string = typewriterElem.innerHTML
@@ -32,13 +32,13 @@ function typewritter (speed) {
     //Putting each word in each div.word
     stringByWords.forEach(function (word) {
         //Creating the div.word
-        wordDiv = createDivWithClass('word')
+        wordDiv = createDivWithClass("word")
         //Insert the word in the div.word
         typewriterContainer.appendChild(wordDiv).innerHTML = word
     })
 
     //Creating div.letter for each cut word 
-    document.querySelectorAll('.word').forEach(function (parent) {
+    document.querySelectorAll(".word").forEach(function (parent) {
         //Cutting the word
         let letters = parent.innerHTML.split("")
         //Removing the word himself
@@ -46,7 +46,7 @@ function typewritter (speed) {
         //Putting each letter in div.letter in each div.word
         letters.forEach(function (child) {
             //Creating the div.letter
-            letterDiv = createDivWithClass('letter')
+            letterDiv = createDivWithClass("letter")
             //Insert the letter in the div.letter
             parent.appendChild(letterDiv).innerHTML = child
         })
@@ -62,18 +62,18 @@ function typewritter (speed) {
     //
 
     /* Display flex */
-    document.querySelectorAll('.letter').forEach(function (letter, index) {
+    document.querySelectorAll(".letter").forEach(function (letter, index) {
         setTimeout(() => {
-            letter.style.display = 'block'
-            letter.style.borderRight = '4px solid #FFCC00'
+            letter.style.display = "block"
+            letter.style.borderRight = "4px solid #FFCC00"
         }, speed * index)
         setTimeout(() => {
-            letter.style.borderRight = '4px solid transparent'
+            letter.style.borderRight = "4px solid transparent"
         }, speed * (index + 1))
     })
     setTimeout(() => {
-        document.querySelector('.word:last-of-type').style.animationPlayState = 'running'
-    }, speed * document.querySelectorAll('.letter').length);
+        document.querySelector(".word:last-of-type").style.animationPlayState = "running"
+    }, speed * document.querySelectorAll(".letter").length);
 }
 
 typewritter(100)
