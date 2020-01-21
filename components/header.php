fb1cd3aed237f01c144695250b5b092026482b12
Marc Bouchez <theparaplux@gmail.com>
2020-01-21 07:45:38 +0100

    [SEO] Ajout des META

--- components/header.php
+++ components/header.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
     <head>
+        <title>Skreen Factory - Soyez visible sur internet dès maintenant en étant accompagné!</title>
+        <meta property="og:site_name" content="Skreen Factory | Web Agency - Web & Seo & Communication">
+        <meta property="og:description" content="Skreen Factory, agence de développement web, consultation seo et communication.">
+        <meta property="og:image" content="">
+        <meta name="description" content="Skreen Factory, agence de développement web, consultation seo et communication.">
+        <meta name="robots" content="index, follow"/>
+        <meta name="googlebot" content="index, follow"/>
+        <link rel="canonical" href="https://skreenfactory.com"/>
+
         <meta charset="utf-8">
-        <title>Skreen Factory</title>
-        <link href="https://fonts.googleapis.com/css?family=Caveat|Long+Cang|Roboto+Slab|Varela+Round&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../resources/css/typewriter.css">
-        <link rel="stylesheet" href="../resources/css/app.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
+        <!-- Internal -->
+        <link rel="stylesheet" href="../resources/css/typewriter.css">
+        <link rel="stylesheet" href="../resources/css/progress-circle.css">
+        <link rel="stylesheet" href="../resources/css/app.css">
+        <!-- External -->
+        <link href="https://fonts.googleapis.com/css?family=Caveat|Long+Cang|Roboto+Slab|Varela+Round&display=swap" rel="stylesheet">
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+        <script type="application/ld+json">
+            {
+            "@context" : "http://schema.org",
+            "@type" : "LocalBusiness",
+            "name" : "Skreen Factory",
+            "telephone" : "06 58 75 52 28",
+            "email" : "marcbouchez.pro@gmail.com",
+            "url" : "https://www.skreenfactory.com/"
+            }
+        </script>
     </head>
     <body>
         <nav class="navbar">
